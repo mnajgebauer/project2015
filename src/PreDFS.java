82ce7498d7a3c376733c2afa749b9e5e0f9a5560
Marcin <marcinmysz@gmail.com>
2015-04-26 11:26:59 +0200

    Test line

--- src/PreDFS.java
+++ src/PreDFS.java
@@ -5,7 +5,7 @@ public class PreDFS<T1, T2 extends NodeInterface<T1>> extends DFS<T1, T2> {
 	public PreDFS(Map<T1, T2> graph) {
 		super(graph);
 	}
-
+	//test line test line
 	@Override
 	protected void preEdge(T1 nodeId, T2 node) {
 		System.out.print(nodeId.toString() + " ");
